013211a7cfcac97d1f30672a50f0d11073f854da
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-03-29 21:19:27 +0530

    adding value to the cart using contructor

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,33 @@ import React from 'react';
 
 // class component always using camel-case convention
 class CartItem extends React.Component {
+    constructor() {
+        // super is used to call the constructor of the parent class if a developer is inheriting
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render () {
+        // object destructuring
+        const {price, title, qty} = this.state;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
                     <img style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
                         {/* button */}
+                        <img alt='increase' className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992651.png'/>
+                        <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/128/992/992683.png'/>
+                        <img alt='delete' className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1214/1214428.png'/>
                     </div>
                 </div>
             </div>
