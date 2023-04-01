31041d04cc657467f3f567e51c97be07003d12d9
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-04-01 09:16:44 +0530

    Add increase decrease and delete function using props

--- src/Cart.js
+++ src/Cart.js
@@ -32,6 +32,41 @@ class Cart extends React.Component {
             ]
         }
     }
+    handleIncreaseQuantity = (product) => {
+        console.log('Hey please inc the qty of', product);
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products
+        })
+    }
+    handleDecreaseQuantity = (product) => {
+        console.log('Hey please inc the qty of', product);
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        if (products[index].qty === 0) {
+            return;
+        }
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        })
+    }
+    handleDeleteProduct = (id) => {
+        const { products } = this.state;
+
+        const items = products.filter((item) => item.id !== id); // [{}]
+        
+        this.setState({
+            products: items
+        })
+    }
     render () {
         const { products } = this.state;
         return(
@@ -42,6 +77,9 @@ class Cart extends React.Component {
                         <CartItem
                              product={product}
                              key={product.id}
+                             onIncreaseQuantity={this.handleIncreaseQuantity}
+                             onDecreaseQuantity={this.handleDecreaseQuantity}
+                             onDeleteProduct={this.handleDeleteProduct}
                         />
                     )
                 })}
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,36 +2,12 @@ import React from 'react';
 
 // class component always using camel-case convention
 class CartItem extends React.Component {
-    // creating an function
-    // by using arrow function it automatically bind the function of their instance i.e cartItem this.increaseQuantity.bind(this)
-    increaseQuantity = () => {
-        console.log('this', this.state);
-        // setState form 1
-
-        this.setState({
-            qty: this.state.qty + 1
-        });
-    }
-
-    decreaseQuantity = () => {
-        const { qty } = this.state;
-
-        if (qty === 0) {
-            return;
-        }
-
-        console.log('this', this.state);
-        // setState form 2 way to write 
-        this.setState ((prevState) => {
-            return {
-                qty: prevState.qty - 1
-            }
-        });
-    }
+    
     render() {
         // object destructuring
         console.log('this.props', this.props);
         const { price, title, qty } = this.props.product;
+        const {product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct} = this.props;
         return (
             <div className='cart-item'>
                 <div className='left-block'>
@@ -47,18 +23,20 @@ class CartItem extends React.Component {
                             alt='increase'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/128/992/992651.png'
-                            onClick={this.increaseQuantity}
+                            onClick={() => onIncreaseQuantity(product)}
                         />
                         <img
                             alt='decrease'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/128/992/992683.png' 
-                            onClick={this.decreaseQuantity}
+                            onClick={() => onDecreaseQuantity(product)}
                         />
                         <img
                             alt='delete'
                             className='action-icons'
-                            src='https://cdn-icons-png.flaticon.com/128/1214/1214428.png' />
+                            src='https://cdn-icons-png.flaticon.com/128/1214/1214428.png' 
+                            onClick={() => onDeleteProduct(product.id)}
+                        />
                     </div>
                 </div>
             </div>
